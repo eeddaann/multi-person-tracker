8846496b934fe01a70d6ad7a9a478593bfd4286b
eeddaann <edan100@gmail.com>
2020-11-07 23:29:29 +0200

    Update mpt.py

--- multi_person_tracker/mpt.py
+++ multi_person_tracker/mpt.py
@@ -24,7 +24,6 @@ class MPT():
             detector_type='yolo',
             yolo_img_size=608,
             output_format='list',
-            notebook=True
     ):
         '''
         Multi Person Tracker
@@ -163,12 +162,12 @@ class MPT():
                 )
                 cv2.putText(img, f'{d[4]}', (d[0] - 9, d[1] - 9), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
                 cv2.putText(img, f'{d[4]}', (d[0] - 8, d[1] - 8), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
-            
+            '''
             if self.notebook:
                 cv2_imshow(img)
             else:
                 cv2.imshow('result video', img)
-
+            '''
             # time.sleep(0.03)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
